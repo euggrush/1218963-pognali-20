3fac6d9dcb48711982cb0617577eb34569ae2393
euggrush <euggrush@gmail.com>
2020-09-14 06:09:41 -0700

    final 3

--- gulpfile.js
+++ gulpfile.js
@@ -46,8 +46,7 @@ const styles = () => {
     .pipe(csso())
     .pipe(rename("styles.min.css"))
     .pipe(sourcemap.write("."))
-    .pipe(gulp.dest("source/css"))
-    //.pipe(gulp.dest("build/css"))
+    .pipe(gulp.dest("build/css"))
     .pipe(sync.stream());
 };
 
